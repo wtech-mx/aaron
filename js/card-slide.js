40aaaa346bfa7496cb6ce6f6eef8b2180e3e26e0
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-04-19 01:23:27 -0500

    upddate slide cards

--- js/card-slide.js
+++ js/card-slide.js
@@ -1,75 +1,36 @@
-$(document).ready(function() {
+$(function (){
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll:1,
+  arrows:false,
+  autoplay:true,
+  autoplaySpeed: 6000,
+  dots: false,
+  centerMode:true,
+  centerPadding:'5px',
 
-$('.owl-carousel').owlCarousel({
-
-    mouseDrag:false,
-    loop:true,
-    margin:2,
-    nav:true,
-
-    responsive:{
-        0:{
-        items:1
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '00px',
+            slidesToShow: 3 ,
+          }
         },
-
-        600:{
-        items:1
-        },
-
-        1000:{
-        items:3
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '5px',
+            slidesToShow: 1 ,
+          }
         }
-    }
+    ]
+
 });
 
-$('.owl-prev').click(function() {
-
-    $active = $('.owl-item .item.show');
-    $('.owl-item .item.show').removeClass('show');
-    $('.owl-item .item').removeClass('next');
-    $('.owl-item .item').removeClass('prev');
-
-    $active.addClass('next');
-
-    if($active.is('.first')) {
-
-        $('.owl-item .last').addClass('show');
-        $('.first').addClass('next');
-        $('.owl-item .last').parent().prev().children('.item').addClass('prev');
-
-    }else {
-
-        $active.parent().prev().children('.item').addClass('show');
-
-        if($active.parent().prev().children('.item').is('.first')) {
-            $('.owl-item .last').addClass('prev');
-        }
-        else {
-            $('.owl-item .show').parent().prev().children('.item').addClass('prev');
-        }
-    }
-});
-
-    $('.owl-next').click(function() {
-        $active = $('.owl-item .item.show');
-        $('.owl-item .item.show').removeClass('show');
-        $('.owl-item .item').removeClass('next');
-        $('.owl-item .item').removeClass('prev');
-        $active.addClass('prev');
-
-    if($active.is('.last')) {
-        $('.owl-item .first').addClass('show');
-        $('.owl-item .first').parent().next().children('.item').addClass('prev');
-
-    }else {
-        $active.parent().next().children('.item').addClass('show');
-
-        if($active.parent().next().children('.item').is('.last')) {
-            $('.owl-item .first').addClass('next');
-        }else {
-            $('.owl-item .show').parent().next().children('.item').addClass('next');
-        }
-    }
-    });
-
-});
+})
